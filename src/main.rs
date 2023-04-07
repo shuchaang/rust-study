55a28d1cde3d54f4ffc2cad8acf7c7ee981b733b
channy <channy@moe.pet>
2023-04-07 11:10:22 +0800

    [init] run

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::collections::HashMap;
 use std::str::FromStr;
-use clap::Parser;
+use clap::{Parser};
 use anyhow::{anyhow, Result};
-use reqwest::Url;
+use colored::*;
+use reqwest::{Url, header, Client, Response};
+use mime::Mime;
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 
 #[derive(Parser,Debug)]
 struct Get {
@@ -54,8 +63,85 @@ struct Opts{
     subcommand: SubCommand
 }
 
-
-fn main() {
-    let opts: Opts = Opts::parse();
-    println!("{:?}",opts);
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
 }
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body =  HashMap::new();
+    for pair in args.body.iter(){
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url)
+        .json(&body)
+        .send()
+        .await?;
+    Ok(print_resp(resp).await?)
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}",resp.version(),resp.status()).blue();
+    println!("{}",status);
+}
+
+fn print_headers(resp: &Response) {
+    for(name,value) in resp.headers(){
+        println!("{}: {:?}",name.to_string().green(),value);
+    }
+    println!();
+}
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        _=> println!("{}", body),
+    }
+}
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime  = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    headers.insert("X-POWERD-BY", "Rust".parse()?);
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcommand{
+        SubCommand::Get(ref args) => get(client,args).await?,
+        SubCommand::Post(ref args) => post(client,args).await?,
+    };
+    Ok(result)
+}
+
